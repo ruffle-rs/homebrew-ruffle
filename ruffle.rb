948238bfd11a3801b8bf12cee16583809d24216b
Jacob Ford <j@cobford.com>
2021-01-04 01:28:28 -0500

    Build from GH repo if --HEAD

--- ruffle.rb
+++ ruffle.rb
@@ -3,9 +3,17 @@ class Ruffle < Formula
   homepage "https://ruffle.rs"
   url "https://github.com/ruffle-rs/ruffle/releases/download/nightly-2021-01-04/ruffle_nightly_2021_01_04_mac.tar.gz"
   sha256 "cd30ac015fa0666d47de4a78f79788a1fb8066114166dd0c99419e33ecf3643e"
+  head "https://github.com/ruffle-rs/ruffle.git"
   license "MIT"
 
+  depends_on "rust" => :build
+
   def install
-    bin.install "ruffle"
+    if build.head?
+      system "cargo", "build", "--package=ruffle_desktop"
+      bin.install "target/debug/ruffle_desktop" => "ruffle"
+    else
+      bin.install "ruffle"
+    end
   end
 end
