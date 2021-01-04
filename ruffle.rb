ced3256d8c4b7dc3023609e2d0b965fdc9739b4e
Jacob Ford <j@cobford.com>
2021-01-04 00:35:26 -0500

    Downloads & symlinks the nightly build, oh joy!

--- ruffle.rb
+++ ruffle.rb
@@ -4,4 +4,8 @@ class Ruffle < Formula
   url "https://github.com/ruffle-rs/ruffle/releases/download/nightly-2021-01-04/ruffle_nightly_2021_01_04_mac.tar.gz"
   sha256 "cd30ac015fa0666d47de4a78f79788a1fb8066114166dd0c99419e33ecf3643e"
   license "MIT"
+
+  def install
+    bin.install "ruffle"
+  end
 end
