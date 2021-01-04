a3ac9049f376666254fc8ec19535d2f06dd16dbe
Jacob Ford <j@cobford.com>
2021-01-04 00:03:52 -0500

    The basic four (with static URL to today's build)

---
+++ ruffle.rb
@@ -0,0 +1,6 @@
+class Ruffle < Formula
+  desc "Flash Player emulator written in Rust"
+  homepage "https://ruffle.rs"
+  url "https://github.com/ruffle-rs/ruffle/releases/download/nightly-2021-01-04/ruffle_nightly_2021_01_04_mac.tar.gz"
+  license "MIT"
+end
