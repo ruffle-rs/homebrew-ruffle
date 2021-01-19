2a6aed821e2f6b5f14b72ed886c1174527772c17
Jacob Ford <j@cobford.com>
2021-01-18 23:55:53 -0500

    Remove specific bottle

--- ruffle.rb
+++ ruffle.rb
@@ -1,8 +1,7 @@
 class Ruffle < Formula
   desc "Flash Player emulator written in Rust"
   homepage "https://ruffle.rs"
-  url "https://github.com/ruffle-rs/ruffle/releases/download/nightly-2021-01-04/ruffle_nightly_2021_01_04_mac.tar.gz"
-  sha256 "cd30ac015fa0666d47de4a78f79788a1fb8066114166dd0c99419e33ecf3643e"
+  url "https://github.com/ruffle-rs/ruffle/"
   head "https://github.com/ruffle-rs/ruffle.git"
   license "MIT"
 
