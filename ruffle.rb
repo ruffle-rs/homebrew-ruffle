00f34ea4a1694a521cbc1d141d758c85d056d9f6
Jacob Ford <j@cobford.com>
2021-01-19 00:03:43 -0500

    HEAD-only bottles can't have a URL

--- ruffle.rb
+++ ruffle.rb
@@ -1,18 +1,13 @@
 class Ruffle < Formula
   desc "Flash Player emulator written in Rust"
   homepage "https://ruffle.rs"
-  url "https://github.com/ruffle-rs/ruffle/"
   head "https://github.com/ruffle-rs/ruffle.git"
   license "MIT"
 
   depends_on "rust" => :build
 
   def install
-    if build.head?
-      system "cargo", "build", "--package=ruffle_desktop"
-      bin.install "target/debug/ruffle_desktop" => "ruffle"
-    else
-      bin.install "ruffle"
-    end
+    system "cargo", "build", "--package=ruffle_desktop"
+    bin.install "target/debug/ruffle_desktop" => "ruffle"
   end
 end
